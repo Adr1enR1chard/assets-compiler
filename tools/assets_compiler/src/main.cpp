3c53128b549af84be664e46c6291db0dfbddc8d7
Adrien RICHARD <adrien.richard.contact@gmail.com>
2026-01-30 17:05:03 +0100

    feat: changing compiler to manage single file

--- tools/assets_compiler/src/main.cpp
+++ tools/assets_compiler/src/main.cpp
@@ -14,72 +14,71 @@ int main(int argc, char **argv)
     // Example: ./asset_compiler /path/to/source /path/to/destination
     if (argc != 3)
     {
-        std::cerr << "Usage: " << argv[0] << " <source_directory> <destination_directory>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <file> <destination_directory>" << std::endl;
         return 1;
     }
 
-    const char *sourceDir = argv[1];
+    const char *sourceFile = argv[1];
     const char *destDir = argv[2];
 
     std::cout << "Asset Compiler Tool" << std::endl;
-    std::cout << "Source Directory: " << sourceDir << std::endl;
+    std::cout << "Source File: " << sourceFile << std::endl;
     std::cout << "Destination Directory: " << destDir << std::endl;
     std::cout << "Starting asset processing..." << std::endl;
 
     // if relative path, make absolute
-    std::filesystem::path sourcePath = std::filesystem::absolute(sourceDir);
+    std::filesystem::path filePath = std::filesystem::absolute(sourceFile);
     std::filesystem::path destPath = std::filesystem::absolute(destDir);
 
-    if (!std::filesystem::exists(sourcePath) || !std::filesystem::is_directory(sourcePath))
+    if (!std::filesystem::exists(filePath) || !std::filesystem::is_regular_file(filePath))
     {
-        std::cerr << "Source directory does not exist or is not a directory: " << sourcePath << std::endl;
+        std::cerr << "Source file does not exist or is not a regular file: " << filePath << std::endl;
         return 1;
     }
 
-    // Iterate through the source directory, process each asset file, and save the optimized version to the destination directory
-    for (const auto &entry : std::filesystem::recursive_directory_iterator(sourcePath))
+    // Process the single source file and save the optimized version to the destination directory
+    if (std::filesystem::is_regular_file(filePath) && filePath.has_extension())
     {
-        if (entry.is_regular_file() && entry.path().has_extension())
+
+        AssetType assetType = GetAssetType(filePath);
+        if (assetType == AssetType::Unknown)
         {
-            const auto &filePath = entry.path();
-
-            AssetType assetType = GetAssetType(filePath);
-            if (assetType == AssetType::Unknown)
-                continue;
-
-            std::filesystem::path relativePath = std::filesystem::relative(filePath, sourcePath);
-            std::filesystem::path outputPath = destPath / relativePath;
-            std::filesystem::create_directories(outputPath.parent_path());
-
-            FILE *outputFile = fopen((outputPath.string() + ".asset").c_str(), "wb");
-            if (!outputFile)
-            {
-                std::cerr << "Failed to open output file: " << outputPath << std::endl;
-                continue;
-            }
-
-            switch (assetType)
-            {
-            case AssetType::Texture:
-                // Ensure the output directory exists
-
-                // Compile the texture
-                CompileTexture(filePath.string().c_str(), outputFile);
-                std::cout << "Compiled texture: " << filePath << " -> " << outputPath << std::endl;
-                break;
-
-            case AssetType::Model:
-                CompileModel(filePath.string().c_str(), outputFile);
-                std::cout << "Compiled model: " << filePath << " -> " << outputPath << std::endl;
-                break;
-
-            default:
-                std::cout << "Unsupported asset type for file: " << filePath << std::endl;
-                break;
-            }
-
-            fclose(outputFile);
+            std::cout << "Unknown asset format, skipping file: " << filePath << std::endl;
+            return 0;
         }
+
+        std::filesystem::path relativePath = std::filesystem::relative(filePath, filePath.parent_path());
+        std::filesystem::path outputPath = destPath / relativePath;
+        std::filesystem::create_directories(outputPath.parent_path());
+
+        FILE *outputFile = fopen((outputPath.string() + ".asset").c_str(), "wb");
+        if (!outputFile)
+        {
+            std::cerr << "Failed to open output file: " << outputPath << std::endl;
+            return 1;
+        }
+
+        switch (assetType)
+        {
+        case AssetType::Texture:
+            // Ensure the output directory exists
+            std::filesystem::create_directories(outputPath.parent_path());
+            // Compile the texture
+            CompileTexture(filePath.string().c_str(), outputFile);
+            std::cout << "Compiled texture: " << filePath << " -> " << outputPath << std::endl;
+            break;
+
+        case AssetType::Model:
+            CompileModel(filePath.string().c_str(), outputFile);
+            std::cout << "Compiled model: " << filePath << " -> " << outputPath << std::endl;
+            break;
+
+        default:
+            std::cout << "Unsupported asset type for file: " << filePath << std::endl;
+            break;
+        }
+
+        fclose(outputFile);
     }
 
     return 0;
